07334c447fae3ee488ed7608f5375a0e317c6d68
superyaz <yacamo04@hotmail.com>
2022-02-26 13:42:58 -0500

    Se crea component Actualizar producto

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
-
+import { ActualizarProductoComponent } from './components/actualizar-producto/actualizar-producto.component';
 
 //componentes
 
@@ -11,12 +11,14 @@ const routes: Routes = [
   { path: '', component: ListarProductosComponent }, 
   { path: 'crear-producto', component: CrearProductoComponent },
   { path: 'crear-producto/:id', component: CrearProductoComponent },
+  { path: 'actualizar-producto', component: ActualizarProductoComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
   
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { ActualizarProductoComponent } from './src/components/actualizar-producto/actualizar-producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearProductoComponent,
-    ListarProductosComponent
+    ListarProductosComponent,
+    ActualizarProductoComponent
   ],
   imports: [
     BrowserModule,
