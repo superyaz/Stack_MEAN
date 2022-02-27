955d1093738ecb211beff5c1232b53097f51ce9c
superyaz <yacamo04@hotmail.com>
2022-02-27 07:35:08 -0500

    Se agregan estilos y se ordenan los componentes

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
-import { ActualizarProductoComponent } from './src/components/actualizar-producto/actualizar-producto.component';
+import { ActualizarProductoComponent } from './components/actualizar-producto/actualizar-producto.component';
 
 @NgModule({
   declarations: [
