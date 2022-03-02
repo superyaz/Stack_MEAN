857e169b64f199184561c434ec0cdd290e70331c
superyaz <yacamo04@hotmail.com>
2022-03-01 22:14:42 -0500

    Se configura el Reactive Form

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
@@ -16,7 +19,8 @@ import { ActualizarProductoComponent } from './components/actualizar-producto/ac
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
